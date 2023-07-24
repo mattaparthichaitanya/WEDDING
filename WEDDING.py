c39bce45469b38ec9349428d6dcd95095f7902c4
chaitanyachaitu227 <121382883+chaitanyachaitu227@users.noreply.github.com>
2023-07-24 07:56:11 +0000

    ok

--- WEDDING.py
+++ WEDDING.py
@@ -7,7 +7,18 @@ from datetime import date
 import os
 import WeddingWaitTime
 from pathlib import Path
+
+###tgen
+import credentials
+from api_helper import ShoonyaApiPy
 api = ShoonyaApiPy()
+ret = api.login(userid=credentials.user, password=credentials.u_pwd, twoFA=credentials.factor2, vendor_code=credentials.vc, api_secret=credentials.app_key, imei=credentials.imei)
+ret = ret['susertoken']
+f = open('TOKEN','w+')
+f.write(ret)
+f.close()
+print("TOKEN GENERATED")
+# api = ShoonyaApiPy()
 today = date.today()
 yesterday = today-timedelta(days=1)
 ###########################
